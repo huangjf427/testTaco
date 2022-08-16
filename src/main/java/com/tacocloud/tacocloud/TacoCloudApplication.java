24c48e1bf6ff9ea5583e3864316de2e54a3d778c
huangjf427 <huamg.jf@163.com>
2022-08-16 20:25:40 +0800

    20220816

--- src/main/java/com/tacocloud/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/tacocloud/tacocloud/TacoCloudApplication.java
@@ -1,19 +1,13 @@
 package com.tacocloud.tacocloud;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-
-@SpringBootApplication
-@RestController
+@SpringBootApplication   // <1>
 public class TacoCloudApplication {
-	@RequestMapping("/")
-	String home() {
-		return "hello";
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(TacoCloudApplication.class, args);
-	}
+
+  public static void main(String[] args) {
+    SpringApplication.run(TacoCloudApplication.class, args); // <2>
+  }
 
 }
